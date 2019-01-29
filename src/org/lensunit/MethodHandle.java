2be4f9808c24dbe569e5b0734fdc28d2f00607eb
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2019-01-29 20:29:16 +0100

    A bit of refactoring. Tentative fix for #1.

---
+++ src/org/lensunit/MethodHandle.java
@@ -0,0 +1,17 @@
+package org.lensunit;
+
+/**
+ * Generic representation of a method to be called
+ * to test that it throws an exception.
+ * 
+ * There might be a better way to do it.
+ * Simply works for now.
+ * 
+ * @author leberre
+ *
+ */
+@FunctionalInterface
+public interface MethodHandle {
+
+	void doSomething() throws Throwable;
+}
--- tst/org/lensunit/Basic.java
+++ tst/org/lensunit/Basic.java
@@ -1,9 +1,12 @@
 package org.lensunit;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Basic extends TestCase {
 
 	public void testTrivialOk() {
-		
+		// an empty test trivially passes
 	}
 	
 	public void testEasyCases() {
@@ -20,6 +23,10 @@ public class Basic extends TestCase {
 		assertEquals(2,4);
 	}
 	
+	public void testError() {
+		throw new NullPointerException();
+	}
+	
 	public void testIntegerValueOf() {
 		Integer a = Integer.valueOf(10);
 		Integer b = Integer.valueOf(10);
@@ -34,5 +41,42 @@ public class Basic extends TestCase {
 		assertNotSame(b,c);
 		assertNotSame(b,d);
 		assertNotSame(c,d);
+	}
+	
+	public void launchException() {
+		throw new IllegalArgumentException();
+	}
+	
+	public void testExpectedException() {
+		assertThrows(IllegalArgumentException.class,this::launchException);
+	}
+	
+	public void testUnexpectedException() {
+		assertThrows(IllegalStateException.class,this::launchException);
+	}
+	
+	public void launchCheckedException() throws IOException {
+		throw new FileNotFoundException();
+	}
+	
+	public void testExpectedCheckedException() {
+		assertThrows(IOException.class,this::launchCheckedException);
+		assertThrows(FileNotFoundException.class,this::launchCheckedException);
+	}
+	
+	public int launchExceptionNonVoidMethod() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void testExceptionOnNonVoidMethod() {
+		assertThrows(UnsupportedOperationException.class,this::launchExceptionNonVoidMethod);
+	}
+	
+	public int launchExceptionNonVoidMethodWithParameters(String s, int n) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void testExceptionOnNonVoidMethodWithParameters() {
+		assertThrows(UnsupportedOperationException.class,() -> this.launchExceptionNonVoidMethodWithParameters("toto",5));
 	}
 }
