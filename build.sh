7e7f3f1933e07b4777aae47fb6eb0b5bdc50ce94
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2019-12-03 17:58:24 +0100

    :package: make jar file Java 8+ compliant

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ find src -name "*.java" -print >javafiles
 if [ ! -d bin ]; then
     mkdir bin
 fi
-javac -d bin -cp bin:lib/dpprocessor.jar @javafiles
+javac -d bin -source 1.8 -target 1.8 -cp bin:lib/dpprocessor.jar @javafiles
 jar -cfm lensunit.jar manifest.mf -C bin/ .
